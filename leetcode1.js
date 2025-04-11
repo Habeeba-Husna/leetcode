2f2df17c98a4b112fc4c21a134a31911bf421597
Habeeba-Husna <habeebahusna@gmail.com>
2025-04-11 15:52:26 +0530

    Initial commit with leetcode1.js

---
+++ leetcode1.js
@@ -0,0 +1,25 @@
+// Write a function to find the longest common prefix string amongst an array of
+// strings.
+// If there is no common prefix, return an empty string "".
+// Example 1:
+// Input: strs = ["flower","flow","flight"]
+// Output: "fl"
+// Example 2:
+// Input: strs = ["dog","racecar","car"]
+// Output: ""
+// Explanation: There is no common prefix among the input strings.
+
+function prefix(strs){
+    if(strs.length==0) return ""
+    let a=strs[0];
+    for(let i=1;i<strs.length;i++){
+    
+        while(strs[i].indexOf(a)!==0){
+            a=a.slice(0,-1)
+            if(a==="") return "";
+        }
+
+    }
+  return a
+
+}
